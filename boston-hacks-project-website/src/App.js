80044fe4ed4bbfab70bacdc31003b6171dd5ddac
lillian chung <lillian@crc-dot1x-nat-10-239-231-171.bu.edu>
2023-11-18 23:08:40 -0500

    input

--- boston-hacks-project-website/src/App.js
+++ boston-hacks-project-website/src/App.js
@@ -1,9 +1,38 @@
 
-
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id="ingredientPrompt">
+    <p>Welcome! Enter the ingredients you want to use, separated by commas:</p>
+    <input type="text" id="ingredientInput" placeholder="e.g., eggs, lettuce, rice"/>
+    <br>
+    </br>
+    <button id="submitBtn1" onclick="getIngredients()">Submit</button>
+
+
+    <p>Now, enter any dietary restrictions or allergies, separated by commas:</p>
+    <input type="text" id="restrictionInput" placeholder="e.g., gluten-free, vegetarian, nut-free"/>
+    <br>
+    </br>
+    <button id="submitBtn2" onclick="getDietaryRestrictions()">Submit</button>
+
     </div>
-  );
+
+  )
+
+  function getingredients(){
+    var ingredientstring = document.getElementById("ingredientInput").value;
+    var ingredients = ingredientstring.split(",");
+    return(ingredients.trim());
+  }
+
+  function getdiet(){
+    var dietstring = document.getElementById("restrictionInput").value;
+    var diet = dietstring.split(",");
+    return(diet.trim());
+  }
+
+
+
 }
+
+export default App;
